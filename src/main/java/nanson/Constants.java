c43aaf55901db870cfdb3960eae2f5cabb13f823
Nanson Chen <buyingsaucer2@gmail.com>
2025-12-09 17:42:38 -0500

    Changed run neurons

--- src/main/java/nanson/Constants.java
+++ src/main/java/nanson/Constants.java
@@ -3,9 +3,9 @@ package nanson;
 public class Constants {
     public static final int DEFAULT_MATRIX_WIDTH = 20;
     public static final int DEFAULT_MATRIX_HEIGHT = 20;
-    public static final int DEFAULT_RUN_NEURONS_PER_CYCLE = (int)(Math.pow(DEFAULT_MATRIX_WIDTH * DEFAULT_MATRIX_HEIGHT, 1.5));
     public static final int DEFAULT_NUM_INCOMING_NEURONS = 2;
     public static final int DEFAULT_LENGTH_OF_RESULTS = 7;
+    public static final int DEFAULT_RUN_NEURONS_PER_CYCLE = DEFAULT_MATRIX_WIDTH * DEFAULT_MATRIX_HEIGHT * DEFAULT_NUM_INCOMING_NEURONS;
     
     // Delimiters for update string format: "row,col,activation;row,col,activation"
     public static final String CELL_DELIMITER = ";";
